4f8ebbde19a741199ece2fbb23bfdc952dd85508
DreamsOfArrakis <brianscottlangdon@gmail.com>
2025-12-19 00:23:13 -0800

    Remove favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,6 @@ const jetbrainsMono = JetBrains_Mono({
 export const metadata: Metadata = {
   title: "Cycle Runner",
   description: "A QA Service on Request - Unlimited tests, fast delivery, fixed monthly rate",
-  icons: {
-    icon: '/favicon.png',
-  },
 };
 
 export default function RootLayout({
